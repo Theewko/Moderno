c67de84fa3b42ff0b95a5d934dbd98f695418a90
Theewko <Theewko1996@mail.ru>
2020-04-19 23:20:05 +0300

    'addedNewPages/Adaptive'

--- app/js/main.js
+++ app/js/main.js
@@ -23,6 +23,19 @@ $(function(){
         to: 600,
         prefix: "$"
     });
+
+
+
+    $('.menu__btn').on('click', function(){
+    $('.menu__list').slideToggle();
+    });
+
+
+    $('.header__btn-menu').on('click', function(){
+    $('.header__box').toggleClass('active');
+    });
+
+
 $('.icon-th-list').on('click', function(){
 $('.product__item').addClass('list');
 $('.icon-th-list').addClass('active');
@@ -35,6 +48,18 @@ $('.product__item').removeClass('list');
 $('.icon-th-large').addClass('active');
 $('.icon-th-list').removeClass('active');
 });
+
+
+$('.product-one__tabs .tab, .settings__tab .tab').on('click', function(event) {
+    var id = $(this).attr('data-id');
+        $('.product-one__tabs, .settings__tab').find('.tab-item').removeClass('active-tab').hide();
+        $('.product-one__tabs .tabs, .settings__tab .tabs').find('.tab').removeClass('active');
+        $(this).addClass('active');
+        $('#'+id).addClass('active-tab').fadeIn();
+        return false;
+    });
+
+
     var mixer = mixitup('.products__inner-box');
 
     
